93d9ed4327d94b1f8bc6cc932fd4e75f2a5caf52
Eduardo <corl.facturacion@gmail.com>
2023-12-28 01:57:07 -0600

    FEAT-Basic express, env & public files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('calendar api backend')
+const express = require("express");
+require("dotenv").config();
+
+//Create express server
+const app = express();
+
+//Public directory
+app.use(express.static("public"));
+
+//Listen request
+app.listen(process.env.PORT, () => {
+  console.log(`Server running in port ${process.env.PORT}`);
+});
