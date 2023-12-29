8b2da7edefda9011cf73ac6714739a77563afb04
Eduardo <corl.facturacion@gmail.com>
2023-12-29 15:02:12 -0600

    FEAT- User DB model

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,34 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
+const User = require("../models/User");
 
-const userRegister = (req, res = response) => {
-  const { name, email, password } = req.body;
+const userRegister = async (req, res = response) => {
+  const { email, password } = req.body;
 
-  res.status(201).json({
-    user: true,
-    msg: "register",
-    name,
-    email,
-    password,
-  });
+  try {
+    let userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({
+        status: "Error",
+        msg: "Email already exist",
+      });
+    }
+
+    const user = new User(req.body);
+
+    await user.save();
+
+    res.status(201).json({
+      user: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      msg: "Internal Server Error",
+    });
+  }
 };
 
 const userLogin = (req, res = response) => {
