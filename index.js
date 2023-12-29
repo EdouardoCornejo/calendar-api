f739bbeb0514b9dff113e06e0f611e2d717db93d
Eduardo <corl.facturacion@gmail.com>
2023-12-28 22:48:05 -0600

    FUNC-Basic auth route

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 //Public directory
 app.use(express.static("public"));
 
+// Routes
+// Auth
+app.use("/api/auth", require("./routes/auth"));
+
+// CRUD,
+
 //Listen request
 app.listen(process.env.PORT, () => {
   console.log(`Server running in port ${process.env.PORT}`);
